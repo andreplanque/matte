0ec9ce9ac08b292dda1c4d59a631c7b396e81bfb
anrde-was-taken <andreplanque@proronmail.com>
2022-05-23 13:25:10 +0200

    Did you ever hear the tragedy of Darth Plagueis The Wise?

--- main.py
+++ main.py
@@ -27,8 +27,20 @@ def main():
     data = Differentiable.from_file('data.csv')  # creating an object from the file
     plt.plot(*data())  # plotting the data
     plt.plot(*data.diff()())  # plotting the derivative
+    plt.plot(*data.diff().diff()())  # plotting the second derivative
     plt.show()  # showing the plot
 
 
 if __name__ == '__main__':
     main()  # calling the main function
+
+    # Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It's not
+    # a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark
+    # Lord of the Sith, so powerful and so wise he could use the Force to influence the
+    # midichlorians to create life... He had such a knowledge of the dark side that he
+    # could even keep the ones he cared about from dying. The dark side of the Force is a
+    # pathway to many abilities some consider to be unnatural. He became so powerful...
+    # The only thing he was afraid of was losing his power, which eventually, of course,
+    # he did. Unfortunately, he taught his apprentice everything he knew, then his
+    # apprentice killed him in his sleep. Ironic. He could save others from death, but
+    # not himself.
