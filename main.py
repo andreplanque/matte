66ba9ec6255e57d0b0f0fc3063d6df2118627261
anrde-was-taken <andreplanque@proronmail.com>
2022-05-23 15:56:29 +0200

    Shit piss fuck

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-# importing necessary modules
-import numpy as np
+import numpy as np  # importing necessary libraries
 import matplotlib.pyplot as plt
+
+
+AMOGUS = np.array([
+        [0, 1, 1, 1],
+        [1, 1, 0, 0],
+        [1, 1, 1, 1],
+        [0, 1, 0, 1]
+    ])
 
 
 class Differentiable:
@@ -23,12 +30,40 @@ class Differentiable:
         return cls(value)
 
 
+class ParseFlights:
+    def __init__(self, path, delimiter=";"):
+        self._path = path
+        self._delimiter = delimiter
+        with open(path, 'r') as f:
+            self._data = np.genfromtxt(f, delimiter=delimiter, unpack=True, dtype=str)
+        self._data = self._data[1:]
+        self._data = self._quadrant_parse(np.array(self._data))
+        self.data = self._data
+
+    def split(self):
+        return np.array([self._data[0][0::4], self._data[1][0::4]]),\
+               np.array([self._data[0][1::4], self._data[1][1::4]]),\
+               np.array([self._data[0][2::4], self._data[1][2::4]]),\
+               np.array([self._data[0][3::4], self._data[1][3::4]])
+
+    @staticmethod
+    @np.vectorize
+    def _quadrant_parse(string):
+        return float(string.replace("K1", ".0").replace("K2", ".25").replace("K3", ".50").replace("K4", ".75"))
+
+
 def main():
+    """
     data = Differentiable.from_file('data.csv')  # creating an object from the file
     plt.plot(*data())  # plotting the data
     plt.plot(*data.diff()())  # plotting the derivative
     plt.plot(*data.diff().diff()())  # plotting the second derivative
     plt.show()  # showing the plot
+    """
+    data = ParseFlights('08510_20220519-103541.csv')
+    # K1, K2, K3, K4 = data.split()
+    plt.plot(*data.data)
+    plt.show()
 
 
 if __name__ == '__main__':
